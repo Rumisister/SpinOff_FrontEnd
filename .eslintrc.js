0c4016e1be8c5cae0efd0cefdc66ec6474947a6b
HyoungDong <52812307+HyoungDong@users.noreply.github.com>
2022-06-19 07:33:18 +0900

    test GCP

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     browser: true,
     node: true,
     es6: true,
+    jest: true,
   },
   extends: ['eslint:recommended', 'plugin:react/recommended'],
   plugins: ['prettier'],
