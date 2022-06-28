a8ddbf0027533206db55e4435d5c480ebd5a0ca0
HyoungDong <52812307+HyoungDong@users.noreply.github.com>
2022-06-28 12:20:25 +0900

    naver social sign_in

--- src/pages/SignInLoadingNaver/SignInLoadingNaver.jsx
+++ src/pages/SignInLoadingNaver/SignInLoadingNaver.jsx
@@ -1,30 +1,41 @@
 import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { axios } from '../../api';
+import { SET_TOKEN } from '../../store/Auth/action';
+import { SUCCESS_SIGNIN } from '../../store/SignIn/action';
 
 function SignInLoadingNaver() {
+  const history = useNavigate();
+  const dispatch = useDispatch();
+  const goBackPage = useSelector(state => state.authReducer.goBackPage);
   useEffect(() => {
     let componentMounted = true;
     const accessCode = new URL(window.location.href).searchParams.get('code');
     console.log(accessCode + '!!');
     const reqeustSocialSignIn = async () => {
       try {
-        const res = axios({
+        const {
+          data: {
+            data: { token, refreshToken, id },
+          },
+        } = axios({
           method: 'post',
           url: '/api/sign/login/social/naver',
           data: {
             authCode: accessCode,
           },
         });
-        console.log(res);
         if (componentMounted) {
-          // dispatch({
-          //   type: SUCCESS_SIGNIN,
-          // });
-          // dispatch({
-          //   type: SET_TOKEN,
-          //   payload: { token, refreshToken, id },
-          // });
-          // history(-3);
+          console.log('로그인 완료');
+          dispatch({
+            type: SUCCESS_SIGNIN,
+          });
+          dispatch({
+            type: SET_TOKEN,
+            payload: { token, refreshToken, id },
+          });
+          history(`${goBackPage}`);
         }
       } catch (error) {
         console.log(error.response);
